45b647927aa871f725c52a33635dbdeeb2917194
IpswichMapper <IpswichMapper@tutanota.com>
2021-02-23 11:18:17 +0000

    fixed bouncing map bug by updating osmdroid

--- app/src/main/java/com/mapitall/SwiftAddress/ChooseBackgroundImagery.kt
+++ app/src/main/java/com/mapitall/SwiftAddress/ChooseBackgroundImagery.kt
@@ -6,11 +6,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class ChooseBackgroundImagery : AppCompatActivity() {
+
+    val TAG = "ChooseBackgroundImagery"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_background_imagery)
+
+        supportActionBar?.title = getString(R.string.choose_background_imagery)
+
     }
 
     // Sends the chosen imagery back to the mainactivity.
@@ -25,5 +32,15 @@ class ChooseBackgroundImagery : AppCompatActivity() {
         intent.putExtra("imagery_chosen", imageryButtonText)
         setResult(RESULT_OK, intent)
         finish()
+    }
+
+    fun changeImagery(view: View) {
+        if (view is ImageView) {
+            Log.i(TAG, "ImageView pressed.")
+
+        } else if (view is Button) {
+
+        }
+
     }
 }
